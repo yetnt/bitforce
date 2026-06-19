ad41dff19ab697c5f6b36085d0b6c713bcf117c0
Lehlogonolo Poole <hlonipoole692@gmail.com>
2026-06-19 13:54:27 +0200

    feat: ClipboardUtil

---
+++ src/main/java/com/yetnt/ut/ClipboardUtil.java
@@ -0,0 +1,23 @@
+package com.yetnt.ut;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Clipboard;
+
+/**
+ * Utility class for interacting with the system clipboard.
+ * Provides methods to copy text to the clipboard.
+ * @author Lehlogonolo Poole
+ */
+public class ClipboardUtil {
+
+    /**
+     * Copies the given text to the system clipboard.
+     *
+     * @param text The string to be copied to the clipboard.
+     */
+    public static void copyToClipboard(String text) {
+        StringSelection selection = new StringSelection(text);
+        java.awt.datatransfer.Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(selection, null);
+    }
+}
